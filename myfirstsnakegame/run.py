eb73075bf655ff6c147f07dad1e9d09bf1b457ec
Michael Kioko <mikeyzmall615@gmail.com>
2024-08-14 12:33:47 +0300

    1st commit

---
+++ myfirstsnakegame/run.py
@@ -0,0 +1,19 @@
+##Michael Kioko first attempt in snake game
+##Hope you enjoy the game 
+##Date 14th/07/2024
+##The file is run.py the main file for the game I use it to run the system or game
+##For more information check my README
+
+
+
+
+
+
+
+
+
+
+
+
+##Michael final code for run.py no errors and it is running well 
+##Have fun
